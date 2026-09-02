4a99564121bfbfeef428534f02f6d199e6eea5b5
thauan.virgilio <thauanvirgiliopessoal@gmail.com>
2026-09-02 08:10:38 -0300

    training map and arrays of objects

---
+++ objects/objectArray.js
@@ -0,0 +1,5 @@
+const produtos = [
+    { id: 1, nome: 'Teclado', preco: 250, ativo: true, categoria: 'periferico' },
+    { id: 2, nome: 'Monitor', preco: 1200, ativo: false, categoria: 'video' },
+    { id: 3, nome: 'Mouse', preco: 90, ativo: true, categoria: 'periferico' },
+];
