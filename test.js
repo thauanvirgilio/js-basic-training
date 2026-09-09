dfb91008e5a79c734a47e0c77be0fcf09f4e1cee
thauan.virgilio <thauanvirgiliopessoal@gmail.com>
2026-09-09 08:23:45 -0300

    training map and for of

--- test.js
+++ test.js
@@ -6,20 +6,40 @@ const products = [
 ];
 
 
-const allProductsNames = products.map(product => product.name)
+// all products filter
+// const allProductsNames = products.map(product => product.name);
 
-// const allProductsNames = products.map(function (product) {
-//     return product.name
+const allProductsNames = products.map(function (product) {
+    return product.name;
+})
+
+// console.log(allProductsNames);
+
+
+// all actives filter
+// const allActives = products.filter(product => product.active === true);
+
+const allActives = products.filter(function (product) {
+    return product.active === true;
+})
+
+// console.log(allActives);
+
+
+// biggest value filter
+value = 0;
+for (const product of products) {
+    if (product.price > value) {
+        value = product.price
+    }
+}
+
+// with map (return index by index)
+// const biggestValue = products.map(function (product) {
+//     if (product.price > value){
+//         value = product.price
+//     }
+//     return value
 // })
 
-console.log(allProductsNames);
-
-
-
-const allActives = products.filter(product => product.active === true);
-
-// const allActives = products.filter(function (product){
-//     return product.active === true;
-// })
-
-console.log(allActives)
+document.getElementById("text").innerHTML = value;
