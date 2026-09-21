7811b849f30751e36de7de365ed24558f42fff1b
thauan.virgilio <thauanvirgiliopessoal@gmail.com>
2026-09-21 16:16:41 -0300

    remove test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-
-const products = [
-    { id: 1, name: 'Keyboard', price: 250, active: true, category: 'pereferic' },
-    { id: 2, name: 'Monitor', price: 1200, active: false, category: 'video' },
-    { id: 3, name: 'Mouse', price: 90, active: true, category: 'pereferic' },
-];
-
-
